b2dc3bbc83d1c73b81ea5fb0ed16f61ba92eb3ae
Mr. Hasan <hasanshahriar32@gmail.com>
2024-10-13 20:10:21 +0600

    Refactor DisqusLoader component to use current page URL for Disqus configuration

--- components/pages/project/DisqusLoader.tsx
+++ components/pages/project/DisqusLoader.tsx
@@ -2,12 +2,13 @@
 import { DiscussionEmbed } from 'disqus-react'
 import React from 'react';
 const DisqusLoader = ({data}:any) => {
+  const pageURL = typeof window !== 'undefined' ? window.location.href : ''
     return (
-      <div className='mt-12'>
+      <div className="mt-12">
         <DiscussionEmbed
           shortname="ece21"
           config={{
-            url: data?.site,
+            url: pageURL,
             identifier: data?.slug,
             title: data?.title,
             language: 'en_US', //e.g. for Traditional Chinese (Taiwan)
