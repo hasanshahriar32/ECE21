1b56c065a64702a7e101bd50aaf3dfda5b5dd896
MR. HASAN <hasanshahriar32@gmail.com>
2024-10-22 01:35:42 +0600

    Update page.tsx

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -16,5 +16,12 @@ export const dynamic = 'force-static'
 export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return (
+    <NextStudio
+      config={config}
+      // Turn off login in production so that anyone can look around in the Studio and see how it works
+      // eslint-disable-next-line no-process-env
+      unstable_noAuthBoundary={process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'}
+    />
+  )
 }
