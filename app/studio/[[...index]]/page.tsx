19b7f786f76a0fa2b950f3171e94f1a1452ba0e0
MR. HASAN <hasanshahriar32@gmail.com>
2025-02-19 12:00:21 +0600

    Update page.tsx

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -21,7 +21,7 @@ export default function StudioPage() {
       config={config}
       // Turn off login in production so that anyone can look around in the Studio and see how it works
       // eslint-disable-next-line no-process-env
-      unstable_noAuthBoundary={process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'}
+      // unstable_noAuthBoundary={process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'}
     />
   )
 }
